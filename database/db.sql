54edd0eb37f2ac317f5ad9e0cd37be6b57f93c6c
KauanFix <kauan_fix@estudante.sesisenai.org.br>
2026-09-02 08:29:06 -0300

    feat - adiciona a tabela de dados no banco de dados

--- database/db.sql
+++ database/db.sql
@@ -2,7 +2,7 @@ CREATE DATABASE IF NOT EXISTS sa_ferrorama ;
 USE sa_ferrorama;
 
 CREATE TABLE IF NOT EXISTS usuarios (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    idUsuario INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(40) NOT NULL,
     email VARCHAR(255) NOT NULL,
     senha VARCHAR(20) NOT NULL,
@@ -10,10 +10,18 @@ CREATE TABLE IF NOT EXISTS usuarios (
 );
 
 CREATE TABLE IF NOT EXISTS sensores (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    idSensor INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(40) NOT NULL,
     localizacao VARCHAR(255) NOT NULL,
     tipo VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS dados(
+    idDado INT AUTO_INCREMENT PRIMARY KEY,
+    valor INT NOT NULL,
+    tipo VARCHAR(20) NOT NULL,
+    idSensor INT NOT NULL,
+    FOREIGN KEY (idSensor) REFERENCES sensores(idSensor)
 );
 
 INSERT INTO usuarios (nome,email,senha,funcao) VALUES ("Admin","admin@gmail.com","123","Administrador");
