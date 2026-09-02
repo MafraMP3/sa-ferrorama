6c3757db02093e49d79035dfdd587fb9e269ba01
KauanFix <kauan_fix@estudante.sesisenai.org.br>
2026-09-02 08:42:52 -0300

    feat - adiciona o atributo de data à tabela dados

--- database/db.sql
+++ database/db.sql
@@ -3,7 +3,7 @@ USE sa_ferrorama;
 
 CREATE TABLE IF NOT EXISTS usuarios (
     idUsuario INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(40) NOT NULL,
+    nome VARCHAR(50) NOT NULL,
     email VARCHAR(255) NOT NULL,
     senha VARCHAR(20) NOT NULL,
     funcao VARCHAR(20) NOT NULL
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS usuarios (
 
 CREATE TABLE IF NOT EXISTS sensores (
     idSensor INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(40) NOT NULL,
+    nome VARCHAR(50) NOT NULL,
     localizacao VARCHAR(255) NOT NULL,
     tipo VARCHAR(20) NOT NULL
 );
@@ -20,6 +20,7 @@ CREATE TABLE IF NOT EXISTS dados(
     idDado INT AUTO_INCREMENT PRIMARY KEY,
     valor INT NOT NULL,
     tipo VARCHAR(20) NOT NULL,
+    dataDado DATE NOT NULL,
     idSensor INT NOT NULL,
     FOREIGN KEY (idSensor) REFERENCES sensores(idSensor)
 );
