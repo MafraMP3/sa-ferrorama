06dbafdbe871a326f4249cdcbbba7708df97c467
KauanFix <kauan_fix@estudante.sesisenai.org.br>
2026-09-04 10:12:45 -0300

    chore: remove código js de verificação de login

--- scripts/script.js
+++ scripts/script.js
@@ -20,77 +20,9 @@ links.forEach(function(link) {
     }
 })
 });
-
-if (!window.location.pathname.includes("index.php")) {
-    if (localStorage.getItem("logado") !== "true") {
-        window.location.replace("index.php");
-    }
-    const logout = document.querySelector('.logout');
-
-    if (logout) {
-        logout.onclick = (e) => {
-
-            e.preventDefault();
-
-            localStorage.clear();
-            window.location.replace("../index.php");
-        }
-    }
-}
-
-//======================================================INDEX.php=======================================================================================//
-
-if (window.location.pathname.includes("index.php")) {
-
-    localStorage.setItem("email", "admin@gmail.com");
-    localStorage.setItem("password", "123");
-
-    const form_login = document.getElementById("form-login");
-
-    if (form_login) {
-        form_login.onsubmit = (e) => {
-
-            e.preventDefault();
-
-            console.log("OI");
-
-            let email = document.getElementById('email').value;
-            let senha = document.getElementById('senha').value;
-            let msg = document.getElementById('mensagem');
-
-            msg.innerHTML = "";
-
-            let valido = true;
-
-            if (senha !== localStorage.getItem('password')) {
-                valido = false;
-                msg.innerHTML = "<div><p id='erro' class='text-danger d-flex justify-content-center'> Senha Inválida </p></div>";
-            }
-
-            if (email !== localStorage.getItem('email')) {
-                valido = false;
-                msg.innerHTML = "<div><p id='erro' class='text-danger d-flex justify-content-center'> Email Inválido </p></div>";
-            }
-
-            if (valido == true) {
-                console.log("Validado");
-                localStorage.setItem("logado", "true");
-                window.location.assign("public/home.php");
-            } else {
-                console.log("Inválido");
-            }
-        }
-    }
-}
-
 //======================================================HOME.php=======================================================================================//
 
 if (window.location.pathname.includes("public/home.php")) {
-
-    let welcome = document.getElementById("saudacao");
-    let nomeAdmin = "Admin";
-
-    welcome.textContent = `Bem-vindo, ${nomeAdmin}`;
 
 }
 //======================================================SENSORES.php=======================================================================================//
