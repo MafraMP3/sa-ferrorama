59795074200f15b8261fd6fd19b7b2544a46b14d
MafraMP3 <caio_a_mafra@estudante.sesisenai.org.br>
2026-09-04 10:07:36 -0300

    refactor - navbar como components incluido em paginas necessarias

--- public/monitoramento.php
+++ public/monitoramento.php
@@ -16,34 +16,9 @@
 
 <!------------------------------------Sidebar---------------------------------------//-->
 
-    <div class="sidebar-overlay" id="overlay" onclick="toggleSidebar()"></div>
-    <button class="hamburger" onclick="toggleSidebar()">
-        <i class="fa-solid fa-bars"></i>
-    </button>
-
-    <div class="sidebar container">
-        <div id="logocontainer">
-            <img src="../assets/images/logo-quadrada.png " alt="logo">
-        </div>
-
-        <a href="home.php" class="sidebar-link" id="linkDashboard">
-            <i class="fa-solid fa-house" style="color: #fff;"></i> Dashboard
-        </a>
-
-        <a href="sensores.php" class="sidebar-link" id="linkSensores">
-            <i class="fa-solid fa-satellite-dish" style="color: #fff;"></i> Sensores
-        </a>
-
-        <a href="#" class="sidebar-link" id="linkMonitoramento" >
-            <i class="fa-solid fa-chart-line" style="color: #fff;"></i> Monitoramento
-        </a>
-
-        <a href="usuarios.php" class="sidebar-link" id="linkUsuarios">
-            <i class="fa-solid fa-users" style="color: rgb(255, 255, 255);"></i> Usuários
-        </a>
-
-        <a href="#" class="logout" id="linkSair">SAIR</a>
-    </div>
+    <?php
+    include "component/navbar.php";
+    ?>
 
 <!------------------------------------------------------------------------------------//-->
 
