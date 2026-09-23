b545a0fe3c4dd9d09d6e1bfe64074ee855197b80
schattenberg-code <lucas_schattenberg@estudante.sesisenai.org.br>
2026-09-23 12:45:48 -0300

    feat - adiciona segurança no cadastro de usuarios

--- public/services/ProcessosUsuario/cadastrarUsuario.php
+++ public/services/ProcessosUsuario/cadastrarUsuario.php
@@ -8,6 +8,30 @@ $senha = $_POST["senha"];
 $cpf = $_POST["cpf"];
 $funcao = $_POST["funcao"];
 
+if (strlen($cpf) < 11) {
+   echo "<script>
+          alert('Erro no cadastro de usuarios, não é permitido cpf menor que 11 digitos');
+          window.location.href = '../../usuarios.php'
+          </script>";
+    die();
+}
+
+if (strlen($nome) < 3) {
+   echo "<script>
+          alert('Erro no cadastro de usuarios, insira um nome valido');
+          window.location.href = '../../usuarios.php'
+          </script>";
+    die();
+}
+
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+   echo "<script>
+          alert('Erro no cadastro de usuarios, insira um email valido');
+          window.location.href = '../../usuarios.php'
+          </script>";
+    die();
+}
+
   if ($nome == null || $email == null || $senha == null || $cpf == null || $funcao == null){
     echo "<script>
           alert('Erro no cadastro de usuarios, não é permitido campos vazios');
